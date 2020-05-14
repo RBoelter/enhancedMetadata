92759a1611d5c6ac881bb41ac621bd40b4e37564
RBoelter <ronny.boelter@outlook.com>
2020-05-14 17:07:56 +0200

    reviewerform

--- EnhancedMetadataSettingsForm.inc.php
+++ EnhancedMetadataSettingsForm.inc.php
@@ -1,24 +1,30 @@
 <?php
 
 import('lib.pkp.classes.form.Form');
-
+import('lib.pkp.classes.file.FileManager');
+import('classes.notification.NotificationManager');
 
 class EnhancedMetadataSettingsForm extends Form
 {
 	public $plugin;
+	private $folderPath;
 
 	public function __construct($plugin)
 	{
 		parent::__construct($plugin->getTemplateResource('settings.tpl'));
+		$contextId = Application::getRequest()->getContext()->getId();
 		$this->plugin = $plugin;
 		$this->addCheck(new FormValidatorPost($this));
 		$this->addCheck(new FormValidatorCSRF($this));
+		$this->folderPath = Config::getVar('files', 'public_files_dir') . '/journals/' . $contextId . '/enhancedForms';
 	}
 
 	public function initData()
 	{
 		$contextId = Application::getRequest()->getContext()->getId();
 		$data = $this->plugin->getSetting($contextId, 'settings');
+		$fileManager = new FileManager();
+		/* TODO READ FILE */
 		if ($data != null && $data != '') {
 			$data = json_decode($data, true);
 			$this->setData('emTitle', $data['title']);
@@ -47,17 +53,33 @@ class EnhancedMetadataSettingsForm extends Form
 			"title" => $this->getData('emTitle'),
 			"file" => $this->getData('emFile')
 		];
-		import('lib.pkp.classes.file.FileManager');
+		$notificationMgr = new NotificationManager();
 		$fileManager = new FileManager();
 
-		$this->plugin->updateSetting($contextId, 'settings', json_encode($data));
-		import('classes.notification.NotificationManager');
-		$notificationMgr = new NotificationManager();
-		$notificationMgr->createTrivialNotification(
-			Application::getRequest()->getUser()->getId(),
-			NOTIFICATION_TYPE_SUCCESS,
-			['contents' => __('common.changesSaved')]
-		);
+		$jsonString = $this->getData('emFile');
+		$jsonObj = json_decode($jsonString, true);
+		if (isset($jsonObj)) {
+			if (!$fileManager->fileExists($this->folderPath))
+				$fileManager->mkdir($this->folderPath);
+			$filepath = $this->folderPath . '/enh_' . $jsonObj['form'] . '_' . $jsonObj['version'] . '.json';
+			if ($fileManager->fileExists($filepath)) {
+				$notificationMgr->createTrivialNotification(
+					Application::getRequest()->getUser()->getId(),
+					NOTIFICATION_TYPE_ERROR,
+					/* TODO correct error msg */
+					['contents' => __('common.error')]
+				);
+			} else {
+				/* TODO SAVE FILENAMESETTINGS TO DB*/
+				$fileManager->writeFile($filepath, $jsonString);
+				$this->plugin->updateSetting($contextId, 'settings', json_encode($data));
+				$notificationMgr->createTrivialNotification(
+					Application::getRequest()->getUser()->getId(),
+					NOTIFICATION_TYPE_SUCCESS,
+					['contents' => __('common.changesSaved')]
+				);
+			}
+		}
 		return parent::execute();
 	}
 }
